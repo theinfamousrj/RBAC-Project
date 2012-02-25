76d309baf86d245b92354ef4c2cd231bccbd64c3
RJ Hill <infamous.rj@gmail.com>
2012-02-25 17:59:09 -0500

    Added methods: size, and getValues to UserRoles and added TestUserRoles class

--- src/edu/westga/cs3152/rbac/UserRoles.java
+++ src/edu/westga/cs3152/rbac/UserRoles.java
@@ -5,12 +5,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author theinfamousrj
+ * @author Raymond Hill
  *
  */
 public class UserRoles {
 	
-	Map<UserId, ArrayList<String>> userRoles;
+	private Map<UserId, ArrayList<String>> userRoles;
 	
 	/**
 	 * Instantiate the userRoles hashmap.
@@ -23,52 +23,52 @@ public class UserRoles {
 	/**
 	 * Add a UserId to this dictionary
 	 * 
-	 * Precondition: userid != null
-	 * Precondition: roles.isEmpty() != true
+	 * Precondition: userid != null && roles.isEmpty() != true && !this.contains(userid)
 	 * 
 	 * @param userid The UserId to be added to this dictionary
 	 * @param roles The ArrayList of role Strings to be added to this dictionary
 	 */
-	public void add (UserId userid, ArrayList<String> roles)
+	public void addWithRoleList (UserId userid, ArrayList<String> roles)
 	{
-		if (userid.toString() == null || roles.isEmpty()) {
+		if (userid.toString() == null || roles.isEmpty() || this.contains(userid)) {
 			return;
 		}
+		// add userid to the collection of UserIds
 		userRoles.put(userid, roles);
 	}
 	
 	/**
 	 * Add a UserId to this dictionary
 	 * 
-	 * Precondition: userid != null
-	 * Precondition: role != null
+	 * Precondition: userid != null && role != null && !this.contains(userid)
 	 *  
 	 * @param userid The UserId to be added to this dictionary
 	 * @param role The singular role String to be added to this dictionary
 	 */
-	public void add (UserId userid, String role)
+	public void addWithRoleString (UserId userid, String role)
 	{
-		if (userid.toString() == null || role == null) {
+		if (userid.toString() == null || role == null || this.contains(userid)) {
 			return;
 		}
 		ArrayList<String> roles = new ArrayList<String>();
 		roles.add(role);
+		// add userid to the collection of UserIds
 		userRoles.put(userid, roles);
 	}
 	
 	/**
 	 * Remove a UserId from this dictionary
 	 * 
-	 * Precondition: userid != null
+	 * Precondition: userid != null && this.contains(userid)
 	 * 
 	 * @param userid The UserId to be removed from this dictionary   
 	 */	
-	private void remove(UserId userid) {
-		
-		if (userid.toString() == null) {
+	public void remove (UserId userid)
+	{
+		if (userid.toString() == null || !this.contains(userid)) {
 			return;
 		}
-		// remove word from the collection of words
+		// remove userid from the collection of UserIds
 		this.userRoles.remove(userid);
 	}
 
@@ -79,10 +79,35 @@ public class UserRoles {
 	 * @param userid The UserId to check if is contained in this dictionary
 	 * @return true iff the specified UserId is contained in this dictionary   
 	 */
-	public boolean contains(UserId userid) {
-		
-		// check whether word is contained in the collection of words
+	public boolean contains(UserId userid)
+	{	
+		// check whether userid is contained in the collection of UserIds
 		return this.userRoles.containsKey(userid);
 	}
 	
+	/**
+	 * Return an ArrayList of Strings as a value with userid as a key
+	 * 
+	 * Precondition: userid != null && this.contains(userid)
+	 * 
+	 * @param userid The UserId to get the ArrayList of values for
+	 * @return
+	 */
+	public ArrayList<String> getValues (UserId userid)
+	{
+		if (userid.toString() == null || !this.contains(userid)) {
+			return null;
+		}
+		return this.userRoles.get(userid);
+	}
+	
+	/**
+	 * Return an integer value indicating the size of the dictionary
+	 * 
+	 * @return the size of the dictionary
+	 */
+	public int getSize ()
+	{
+		return this.userRoles.size();
+	}
 }
--- tests/edu/westga/cs3152/rbac/UserRolesTest.java
+++ tests/edu/westga/cs3152/rbac/UserRolesTest.java
@@ -2,6 +2,9 @@ package edu.westga.cs3152.rbac;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -10,10 +13,106 @@ import org.junit.Test;
  */
 public class UserRolesTest {
 
+	ArrayList<String> roleList;
+	UserRoles roles;
+	UserId userid;
+	UserId userid2;
+	UserId userid3;
+	
+	@Before
+	public void Setup() {
+		roles = new UserRoles();
+		userid = new UserId("csm65490");
+		userid2 = new UserId("ctm21392");
+		userid3 = new UserId("del24690");
+		roleList = new ArrayList<String>();
+	}
+	
+	//Testing add()
+	
 	@Test
-	public void shouldNotMatchNull() {
-		Rule aRule = new Rule("physician", "drug", Rule.READ);
-		assertFalse(aRule.matches(null));
+	public void shouldNotContainAnyUserIdWhenGivenNullRoleList() {
+		roles.addWithRoleList(userid, roleList);
+		assertFalse(roles.contains(userid));
+	}
+	
+	@Test
+	public void shouldContainCorrectUserIdAndRoleWhenGivenUserIdAndRoleAsAString() {
+		String nurseStr = "nurse";
+		addARoleToList(nurseStr);
+		roles.addWithRoleString(userid, nurseStr);
+		assertTrue(roles.contains(userid) && roles.getValues(userid).equals(roleList));
+	}
+	
+	@Test
+	public void shouldContainCorrectUserIdAndRoleWhenGivenUserIdAndRoleAsAnArrayList() {
+		addARoleToList("nurse");
+		roles.addWithRoleList(userid, roleList);
+		assertTrue(roles.contains(userid) && roles.getValues(userid).equals(roleList));
+	}
+	
+	@Test
+	public void shouldContainOnlyOneUserIdWhenAddMultipleEqualUserIds() {
+		addARoleToList("nurse");
+		roles.addWithRoleList(userid, roleList);
+		roles.addWithRoleList(userid, roleList);
+		roles.addWithRoleList(userid, roleList);
+		assertTrue(roles.contains(userid) && roles.getSize() == 1);
+	}
+	
+	//Testing getValues()
+	
+	@Test
+	public void shouldHaveMultipleRolesForUserIdWhenAddedWithMultipleRoles() {
+		addARoleToList("nurse");
+		addARoleToList("doctor");
+		addARoleToList("visitor");
+		roles.addWithRoleList(userid, roleList);
+		assertTrue(roles.contains(userid) && roles.getValues(userid).equals(roleList));
+	}
+	
+	//Testing contains()
+	
+	@Test
+	public void shouldNotContainAnyUserIdWhenGivenNullRoles() {
+		roles.addWithRoleString(userid, null);
+		assertFalse(roles.contains(userid));
+	}
+	
+	@Test
+	public void shouldContainThreeUserIdsWhenAddThreeUserIds() {
+		addARoleToList("nurse");
+		roles.addWithRoleList(userid, roleList);
+		roles.addWithRoleList(userid2, roleList);
+		roles.addWithRoleList(userid3, roleList);
+		assertTrue(roles.contains(userid) && roles.contains(userid2) && roles.contains(userid3) && roles.getSize() == 3);
+	}
+	
+	//Testing remove()
+	
+	@Test
+	public void shouldRemoveCorrectUserIdAndRoleWhenGivenUserIdToRemove() {
+		addARoleToList("nurse");
+		roles.addWithRoleList(userid, roleList);
+		roles.addWithRoleList(userid2, roleList);
+		roles.addWithRoleList(userid3, roleList);
+		boolean didContainUserId = roles.contains(userid);
+		roles.remove(userid);
+		assertTrue(!roles.contains(userid) && didContainUserId);
+	}
+	
+	@Test
+	public void shouldNotRemoveAnyUserIdWhenRemovingUserIdThatIsNotInDictionary() {
+		addARoleToList("nurse");
+		roles.addWithRoleList(userid, roleList);
+		roles.remove(userid2);
+		assertTrue(roles.contains(userid) && roles.getSize() == 1);
+	}
+	
+	//Helper methods
+	
+	public void addARoleToList(String roleStr) {
+		roleList.add(roleStr);
 	}
 	
 }
